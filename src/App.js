ddd707d27c67889ecfc98e2ca7056113f52ab5dc
PetroDutka03 <dutkapetro0372@gmail.com>
2020-11-23 12:22:18 +0200

    something

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=Kbj2Zss-5GY&list=RDy_5azy1KMv0&index=11"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
